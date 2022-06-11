ef840712928aaf113b94e693d823100af9b99d85
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2022-06-12 01:56:06 +1000

    change nixos-rebuild alias

--- zsh/shellInit.zsh
+++ zsh/shellInit.zsh
@@ -7,5 +7,5 @@ function myClear {
 myClear
 alias clear=myClear
 alias nr="nixos-rebuild switch --flake ~/flakes/nixos/#anthony --use-remote-sudo"
-alias nu="nixos-rebuild switch --upgrade --flake ~/flakes/nixos/#anthony --use-remote-sudo"
+alias nu="nix flake update ~/flakes/nixos && nixos-rebuild switch --flake ~/flakes/nixos/#anthony --use-remote-sudo"
 export TERM=xterm-256color
