60029b1a5925fede3a50f70f36b5a45f4f48c73c
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2022-06-12 16:26:57 +1000

    add ranger cd function, refactor clear

--- zsh/shellInit.zsh
+++ zsh/shellInit.zsh
@@ -1,9 +1,23 @@
-function myClear {
+function clear {
   for i in {2..$(tput lines)}
   do
     echo ""
   done
 }
-myClear
-alias clear=myClear
+function ranger {
+  local IFS=$'\t\n'
+  local tempfile="$(mktemp -t tmp.XXXXXX)"
+  local ranger_cmd=(
+    command
+    ranger
+    --cmd="map Q chain shell echo %d > "$tempfile"; quitall"
+  )
+
+  ${ranger_cmd[@]} "$@"
+  if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]]; then
+    cd -- "$(cat "$tempfile")" || return
+  fi
+  command rm -f -- "$tempfile" 2>/dev/null
+}
+clear
 export TERM=xterm-256color
